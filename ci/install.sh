78fe0feb0ddd38eeef82094c8f8775a1bb871308
liam beckman <beckmanl@ohsu.edu>
2020-06-03 09:44:35 -0700

    Update travis script.

--- ci/install.sh
+++ ci/install.sh
@@ -1,24 +1,12 @@
 #!/bin/sh
 set -ex
 
-if [ $TRAVIS_OS_NAME = 'linux' ]
+if [ $TRAVIS_OS_NAME = 'osx' ]
 then
-    sudo apt-get update
-    sudo apt-get install --yes \
-        gcc \
-        g++ \
-        libfontconfig1-dev \
-        libgraphite2-dev \
-        libharfbuzz-dev \
-        libicu-dev \
-        libssl-dev \
-        zlib1g-dev
-
-elif [ $TRAVIS_OS_NAME = 'osx' ]
-then
+    export OPENSSL_INCLUDE_DIR=$(brew --prefix openssl)/include
+    export OPENSSL_LIB_DIR=$(brew --prefix openssl)/lib
     export DEP_OPENSSL_INCLUDE=$(brew --prefix openssl)/include
     export PKG_CONFIG_PATH=/usr/local/opt/icu4c/lib/pkgconfig
-    brew install --only-dependencies tectonic
 fi
 
 rustup self update
