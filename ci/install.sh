24d9a98c586f5d33245adfd4e6bd34d746310d7d
liam beckman <beckmanl@ohsu.edu>
2020-06-02 19:41:49 -0700

    Clean up ci directory.

--- ci/install.sh
+++ ci/install.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 set -ex
 
-apt-get update
-apt-get install --yes \
-    curl \
-    gcc \
-    g++ \
-    libfontconfig1-dev \
-    libgraphite2-dev \
-    libharfbuzz-dev \
-    libicu-dev \
-    libssl-dev \
-    zlib1g-dev
+if [ $TRAVIS_OS_NAME = 'linux' ]
+then
+    apt-get update
+    apt-get install --yes \
+        gcc \
+        g++ \
+        libfontconfig1-dev \
+        libgraphite2-dev \
+        libharfbuzz-dev \
+        libicu-dev \
+        libssl-dev \
+        zlib1g-dev
 
-#curl https://sh.rustup.rs -sSf | sh -s -- -y
+elif [ $TRAVIS_OS_NAME = 'osx' ]
+then
+    export DEP_OPENSSL_INCLUDE=$(brew --prefix openssl)/include
+    export PKG_CONFIG_PATH=/usr/local/opt/icu4c/lib/pkgconfig
+    brew install --only-dependencies tectonic
+    brew install pkg-config
+fi
+
 rustup self update
 . $HOME/.cargo/env
 cargo install mdbook-latex
+
