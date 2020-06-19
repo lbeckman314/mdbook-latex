942f50d9136c6ce4ef58721786e1ef6f532304ae
liam beckman <beckmanl@ohsu.edu>
2020-06-18 18:06:27 -0700

    Update travis script.

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,8 +1,33 @@
-# This script takes care of building your crate and packaging it for release
-
+#!/bin/sh
 set -ex
 
+# This script takes care of building your crate and packaging it for release
 main() {
+    install
+    build_docs
+    package
+}
+
+install() {
+    if [ $TRAVIS_OS_NAME = 'osx' ]
+    then
+        export OPENSSL_INCLUDE_DIR=$(brew --prefix openssl)/include
+        export OPENSSL_LIB_DIR=$(brew --prefix openssl)/lib
+        export DEP_OPENSSL_INCLUDE=$(brew --prefix openssl)/include
+        export PKG_CONFIG_PATH=/usr/local/opt/icu4c/lib/pkgconfig
+    fi
+
+    rustup self update
+    . $HOME/.cargo/env
+    cargo build --release --target $TARGET
+}
+
+build_docs() {
+    cargo install mdbook
+    "$HOME/.cargo/bin/mdbook build docs"
+}
+
+package() {
     local src=$(pwd) \
           stage=
 
@@ -27,3 +52,4 @@ main() {
 }
 
 main
+
