1817a38c76611b05bfec3604c241289136026aa6
Mateusz Szczygieł <msz@genesismobo.com>
2021-11-02 14:50:21 +0100

    fix clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,19 @@
-#[cfg(feature = "latex")]
-extern crate md2tex;
-extern crate mdbook;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-#[cfg(feature = "pdf")]
-extern crate tectonic;
-
-#[cfg(feature = "md2tex")]
-use md2tex::markdown_to_tex;
 use mdbook::book::BookItem;
 use mdbook::config::Config as MdConfig;
 use mdbook::renderer::RenderContext;
+
+#[cfg(feature = "latex")]
+use std::fmt::Write as FmtWrite;
+
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
-use tectonic::status::plain::PlainStatusBackend;
-use tectonic::status::ChatterLevel;
+
+#[cfg(feature = "pdf")]
+use tectonic::status::{plain::PlainStatusBackend, ChatterLevel};
+
+use serde::{Deserialize, Serialize};
 
 // config definition.
 #[derive(Debug, Default, Serialize, Deserialize)]
@@ -48,47 +44,49 @@ pub fn generate(ctx: &RenderContext) -> std::io::Result<()> {
         None => panic!("Could not read configuration."),
     };
 
-    // Read book's config values (title, authors).
-    let title = ctx.config.book.title.as_ref().unwrap();
-    let authors = ctx.config.book.authors.join(" \\and ");
-
-    // Copy template data into memory.
-    let mut template = if let Some(custom_template) = cfg.custom_template {
-        let mut custom_template_path = ctx.root.clone();
-        custom_template_path.push(custom_template);
-        std::fs::read_to_string(custom_template_path)?
-    } else {
-        include_str!("template.tex").to_string()
-    };
-
-    // Add title and author information.
-    template = template.replace(r"\title{}", &format!("\\title{{{}}}", title));
-    template = template.replace(r"\author{}", &format!("\\author{{{}}}", authors));
-
-    // Iterate through markdown source.
-    let mut content = String::new();
-    for item in ctx.book.iter() {
-        // Iterate through each chapter.
-        if let BookItem::Chapter(ref ch) = *item {
-            if cfg.ignores.contains(&ch.name) {
-                continue;
-            }
-
-            content.push_str(&ch.content);
-        }
-    }
-
     if cfg.markdown {
         // Output markdown file.
         let filename = output_filename(&ctx.destination, &ctx.config, "md");
+
+        // Iterate through markdown source.
+        let mut content = String::new();
+        for item in ctx.book.iter() {
+            // Iterate through each chapter.
+            if let BookItem::Chapter(ref ch) = *item {
+                if cfg.ignores.contains(&ch.name) {
+                    continue;
+                }
+
+                content.push_str(&ch.content);
+            }
+        }
+
         write_file(&content, filename);
     }
 
     #[cfg(feature = "latex")]
     {
+        // Read book's config values (title, authors).
+        let title = ctx.config.book.title.as_ref().unwrap();
+        let authors = ctx.config.book.authors.join(" \\and ");
+
+        // Copy template data into memory.
+        let mut template = if let Some(custom_template) = &cfg.custom_template {
+            let mut custom_template_path = ctx.root.clone();
+            custom_template_path.push(custom_template);
+            std::fs::read_to_string(custom_template_path)?
+        } else {
+            include_str!("template.tex").to_string()
+        };
+
+        // Add title and author information.
+        template = template
+            .replace(r"\title{}", &format!("\\title{{{}}}", title))
+            .replace(r"\author{}", &format!("\\author{{{}}}", authors));
+
         let mut latex = String::new();
         if cfg.latex || cfg.pdf {
-            latex = get_latex(content, template);
+            latex = get_latex(ctx, &cfg, template);
         }
 
         // Output latex file.
@@ -116,7 +114,7 @@ fn write_pdf(latex: String, filename: PathBuf) {
     let sb = PlainStatusBackend::new(ChatterLevel::Normal);
     let data: Vec<u8> = latex_to_pdf(&latex, sb).expect("processing failed");
     let mut output = File::create(filename).unwrap();
-    output.write(&data).unwrap();
+    output.write_all(&data).unwrap();
 }
 
 #[cfg(feature = "pdf")]
@@ -171,20 +169,57 @@ pub fn latex_to_pdf<T: AsRef<str>, S: tectonic::status::StatusBackend>(
 }
 
 #[cfg(feature = "latex")]
-fn get_latex(content: String, template: String) -> String {
+fn get_latex(ctx: &RenderContext, cfg: &Config, mut template: String) -> String {
+    let asset_prefix = ctx
+        .destination
+        .strip_prefix(&ctx.root)
+        .map(|path| path.iter().map(|_| "..").collect())
+        .unwrap_or_else(|_| ctx.root.clone())
+        .join("src");
+
+    // Iterate through markdown source.
+    let mut content = String::new();
+
+    fn for_each_chap(
+        content: &mut String,
+        asset_prefix: &Path,
+        cfg: &Config,
+        items: &[BookItem],
+        level: i32,
+    ) {
+        for item in items {
+            if let BookItem::Chapter(ref ch) = *item {
+                if cfg.ignores.contains(&ch.name) {
+                    continue;
+                }
+
+                let prefix = asset_prefix.join(ch.path.as_ref().and_then(|p| p.parent()).unwrap());
+                let latex = md2tex::Converter::new(&ch.content)
+                    .assets(&prefix)
+                    .chapter_level_offset(level)
+                    .run();
+
+                writeln!(content, "{}", latex).unwrap();
+
+                for_each_chap(content, asset_prefix, cfg, &ch.sub_items, level + 1);
+            }
+        }
+    }
+
+    for_each_chap(&mut content, &asset_prefix, cfg, &ctx.book.sections, 0);
+
     // Insert new LaTeX data into template after "%% mdbook-latex begin".
     let begin = "mdbook-latex begin";
     let target = template.find(&begin).unwrap() + begin.len();
-    let mut output = template.clone();
-    let latex = markdown_to_tex(content.clone());
-    output.insert_str(target, &latex);
-    output
+
+    template.insert_str(target, &content);
+    template
 }
 
 /// Output plain text file.
 ///
 /// Used for writing markdown and latex data to files.
-fn write_file(data: &String, filename: PathBuf) {
+fn write_file(data: &str, filename: PathBuf) {
     let display = filename.display();
 
     let mut file = match File::create(&filename) {
@@ -193,9 +228,8 @@ fn write_file(data: &String, filename: PathBuf) {
     };
 
     // Write to file.
-    match file.write_all(data.as_bytes()) {
-        Err(why) => panic!("Couldn't write to {}: {}", display, why.to_string()),
-        Ok(_) => (),
+    if let Err(why) = file.write_all(data.as_bytes()) {
+        panic!("Couldn't write to {}: {}", display, why.to_string())
     }
 }
 
