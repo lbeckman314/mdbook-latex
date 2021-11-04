6ab0e9f41155bd45ca847901fb9cc90c96005a0d
Mateusz Szczygieł <msz@genesismobo.com>
2021-11-04 13:34:28 +0100

    add test case for building The Rust Programing Language book

---
+++ tests/rust-book/build.sh
@@ -0,0 +1,19 @@
+#! /bin/env bash
+
+if [ ! -d "book" ]
+then
+	git clone --depth=1 https://github.com/rust-lang/book.git
+	echo '
+	[output.latex]
+	pdf = true
+	latex = true
+	custom-template = "../template.tex"
+	' >> book/book.toml
+fi
+
+cd book || exit
+
+
+mdbook build
+
+cp book/latex/*.pdf ..
