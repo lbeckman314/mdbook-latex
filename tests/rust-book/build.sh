be16b68597b9bbb5fe8bf47c3987caf07572ff49
Mateusz Kondej <mk@genesismobo.com>
2021-11-04 13:40:24 +0100

    test rust book

--- tests/rust-book/build.sh
+++ tests/rust-book/build.sh
@@ -1,14 +1,17 @@
 #! /bin/env bash
 
+set -e
+
 if [ ! -d "book" ]
 then
 	git clone --depth=1 https://github.com/rust-lang/book.git
 	echo '
-	[output.latex]
-	pdf = true
-	latex = true
-	custom-template = "../template.tex"
-	' >> book/book.toml
+[output.latex]
+pdf = true
+latex = true
+custom-template = "../template.tex"
+' >> book/book.toml
+
 fi
 
 cd book || exit
